9ef52f50fdcb44843c344546f8e588cabc89786e
Reino17 <rwijnsma@xs4all.nl>
2019-04-27 16:13:01 +0200

    curl 7.63.0 --> 7.64.1

--- ffmpeg_local_builds/cross_compile_ffmpeg.sh
+++ ffmpeg_local_builds/cross_compile_ffmpeg.sh
@@ -1373,8 +1373,8 @@ build_curl() {
   #build_openssl-1.0.2
   #build_openssl-1.1.1
 
-  download_and_unpack_file https://curl.haxx.se/download/curl-7.63.0.tar.bz2
-  cd curl-7.63.0
+  download_and_unpack_file https://curl.haxx.se/download/curl-7.64.1.tar.bz2
+  cd curl-7.64.1
     generic_configure "--without-ssl --with-mbedtls --with-ca-bundle=ca-bundle.crt" # --with-ca-fallback only works with OpenSSL or GnuTLS.
     do_make # 'curl.exe' only. No install.
     do_strip src/curl.exe
@@ -1384,7 +1384,7 @@ build_curl() {
     fi
 
     mkdir -p $redist_dir # Pack 'curl.exe'.
-    archive="$redist_dir/curl-7.63.0_mbedtls_zlib-win32-static"
+    archive="$redist_dir/curl-7.64.1_mbedtls_zlib-win32-static"
     if [[ $original_cflags =~ "pentium3" ]]; then
       archive+="_legacy"
     fi
