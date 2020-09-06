07892c82bcfb607929c9e7605db89933bb13dad1
Reino17 <rwijnsma@xs4all.nl>
2020-09-06 22:37:50 +0200

    build_lame: 'libmp3lame_fix-nasm-compilation.patch' no longer necessary
    Patched upstream.

--- ffmpeg_local_builds/cross_compile_ffmpeg.sh
+++ ffmpeg_local_builds/cross_compile_ffmpeg.sh
@@ -603,7 +603,6 @@ build_libopus() {
 build_lame() {
   do_svn_checkout https://svn.code.sf.net/p/lame/svn/trunk/lame lame_svn
   cd lame_svn
-    apply_patch file://$patch_dir/libmp3lame_fix-nasm-compilation.patch # Fix NASM compilation error by changing 'nasm.h' from UTF-8 BOM to UTF-8 and mute several warnings. See https://sourceforge.net/p/lame/patches/81/.
     if [[ ! -f Makefile.in.bak ]]; then # Library only.
       sed -i.bak "/^SUBDIRS/s/ frontend//;/^SUBDIRS/s/ doc//" Makefile.in
     fi
