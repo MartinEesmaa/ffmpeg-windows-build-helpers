644322d998655793f609f6eb7e449e2056a463c7
Reino17 <rwijnsma@xs4all.nl>
2021-04-28 00:23:21 +0200

    build_curl: curl 7.72.0 --> 7.74.0
    And don't rename CA cert file.

--- ffmpeg_local_builds/cross_compile_ffmpeg.sh
+++ ffmpeg_local_builds/cross_compile_ffmpeg.sh
@@ -998,29 +998,29 @@ build_openssl-dlls() {
 }
 
 build_curl() {
-  download_and_unpack_file https://curl.haxx.se/download/curl-7.72.0.tar.bz2
+  download_and_unpack_file https://curl.se/download/curl-7.74.0.tar.bz2
   if [ "$1" = "openssl" ]; then # Compile Curl with OpenSSL for hlsdl.
     build_openssl-1.1.1 static
-    cd curl-7.72.0
+    cd curl-7.74.0
     export PKG_CONFIG="pkg-config --static" # Automatically detect all of OpenSSL its dependencies.
     generic_configure --without-ca-bundle --with-ca-fallback
     unset PKG_CONFIG
     do_make install-strip
   else # Compile Curl with MbedTLS and create archive.
     build_mbedtls
-    cd curl-7.72.0
-    generic_configure --without-ssl --with-mbedtls --with-ca-bundle=ca-bundle.crt LDFLAGS=-s # --with-ca-fallback only works with OpenSSL or GnuTLS.
+    cd curl-7.74.0
+    generic_configure --without-ssl --with-mbedtls --with-ca-bundle=cacert.pem LDFLAGS=-s # --with-ca-fallback only works with OpenSSL or GnuTLS.
     do_make # 'curl.exe' only. No install.
-    if [[ ! -f ca-bundle.crt ]]; then # For 'ca-bundle.crt' see https://superuser.com/a/442797.
-      echo -e "\e[1;33mDownloading 'https://curl.haxx.se/ca/cacert.pem' and renaming to 'ca-bundle.crt'.\e[0m"
-      curl -o ca-bundle.crt https://curl.haxx.se/ca/cacert.pem
+    if [[ ! -f cacert.pem ]]; then # See https://curl.se/docs/sslcerts.html and https://superuser.com/a/442797 for more on the CA cert file.
+      echo -e "\e[1;33mDownloading 'https://curl.haxx.se/ca/cacert.pem'.\e[0m"
+      curl -O https://curl.haxx.se/ca/cacert.pem
     fi
 
     mkdir -p $redist_dir
-    archive="$redist_dir/curl-7.72.0_mbedtls_zlib-win32-static-xpmod-sse"
+    archive="$redist_dir/curl-7.74.0_mbedtls_zlib-win32-static-xpmod-sse"
     if [[ ! -f $archive.7z ]]; then # Pack static 'curl.exe'.
       sed "s/$/\r/" COPYING > COPYING.txt
-      7z a -mx=9 -bb3 $archive.7z ./src/curl.exe ca-bundle.crt COPYING.txt
+      7z a -mx=9 -bb3 $archive.7z ./src/curl.exe cacert.pem COPYING.txt
       rm -v COPYING.txt
     else
       echo -e "\e[1;33mAlready made '${archive##*/}.7z'.\e[0m"
