e5d6f2feb16972d29ad412663984dacdc3b23f67
Reino17 <rwijnsma@xs4all.nl>
2024-04-07 19:00:07 +0200

    build_ffms2_cplugin: Add option to build statically
    Restrict git revision to 34a4379. See https://forum.doom9.org/showthread.php?p=1995732#post1995732.

--- ffmpeg_local_builds/cross_compile_ffmpeg.sh
+++ ffmpeg_local_builds/cross_compile_ffmpeg.sh
@@ -1126,8 +1126,20 @@ build_hlsdl() {
 } # curl(openssl)
 
 build_ffms2_cplugin() {
-  build_ffmpeg shared
-  do_git_checkout https://github.com/qyot27/ffms2_cplugin.git "" c_plugin
+  if [ "$1" = "static_ffmpeg" ]; then
+    do_git_checkout https://github.com/FFmpeg/FFmpeg.git "" "" 238f9de876c4298606ce41992e16b959d108b633
+    cd FFmpeg_git
+      ff_rev=$(git describe --tags | tail -c +2 | sed 's/dev-//;s/g//')
+      apply_patch $patch_dir/0001-make-bcrypt-optional.patch -p1
+      do_configure --arch=x86 --target-os=mingw32 --prefix=$mingw_w64_x86_64_prefix --cross-prefix=$cross_prefix --extra-cflags="$CFLAGS" --pkg-config=pkg-config --pkg-config-flags=--static --enable-gpl --enable-version3 --disable-bcrypt --disable-debug --disable-doc --disable-htmlpages --disable-manpages --disable-podpages --disable-schannel --disable-txtpages --disable-w32threads --disable-avdevice --disable-avfilter --disable-devices --disable-encoders --disable-filters --disable-hwaccels --disable-mediafoundation --disable-muxers --disable-network --disable-programs --disable-sdl2 --enable-libaom
+      do_make
+      do_make_install
+    cd ..
+  else
+    build_ffmpeg shared
+  fi
+
+  do_git_checkout https://github.com/qyot27/ffms2_cplugin.git "" c_plugin 34a4379c211b166da50bf3a6bfb249166634547d
   cd ffms2_cplugin_git
     apply_patch $patch_dir/ffms2_configure-fix-various.patch -p1 # Correctly detect MingW32, use Cygwin's pkg-config and don't set GCC optimization level twice if $CFLAGS already contains one.
     if [[ ! -f src/core/ffms.cpp.bak ]]; then
@@ -1140,9 +1152,12 @@ build_ffms2_cplugin() {
 
     mkdir -p $redist_dir
     archive="$redist_dir/ffms2-$(git describe --tags | sed 's/g//')-avs-vsp-win32-shared-xpmod-sse"
+    if [ "$1" = "static_ffmpeg" ]; then
+      archive="${archive}_ffmpeg-$ff_rev"
+    fi
     if [[ ! -f $archive.7z ]]; then
       sed "s/$/\r/" etc/COPYING.GPLv3 > COPYING.GPLv3.txt
-      7z a -mx=9 -bb3 $archive.7z ffms3.dll ffmsindex.exe ./etc/FFMS2.avsi doc COPYING.GPLv3.txt
+      7z a -mx=9 -bb3 $archive.7z ffms3.dll ffmsindex.exe ./etc/FFMS2-cplugin.avsi doc COPYING.GPLv3.txt
       rm -v COPYING.GPLv3.txt
     else
       echo -e "\e[1;33mAlready made '${archive##*/}.7z'.\e[0m"
